898223b6638b238b89aa9e72e7d32feea43f56db
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-19 03:03:59 +0530

    - filtering queries: http://localhost:8000/api/books?genre=Science%20Fiction

--- bookapi/app.js
+++ bookapi/app.js
@@ -14,14 +14,15 @@ var bookRouter = express.Router();
 
 bookRouter.route('/Books')
     .get(function (req, res) {
+        var query = req.query;
 
-        Book.find(function (err, books) {
+        Book.find(query, function (err, books) {
             if (err)
                 res.status(500).send(err);
             else
                 res.json(books);
         });
-        
+
     });
 
 app.use('/api', bookRouter);
