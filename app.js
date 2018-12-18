1ae96f5f4c2fc1f5fab6642fadc868023214cc8c
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-19 00:39:10 +0530

    - res not req!

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-    req.send('welcome to my API!');
+    res.send('welcome to my API!');
 });
 
 app.listen(port, function () {
