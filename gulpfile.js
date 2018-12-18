7c02c3b683308b3be79a3ea49a0ff0d13601ca38
Adarsh Maurya <adarshmaurya@softhinkers.com>
2018-12-19 00:52:21 +0530

    - adding gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,19 @@
-var gulp = require('gulp'); //gulp is mere a task runner
+var gulp = require('gulp'),
+    nodemon = require('gulp-nodemon'); //gulp is mere a task runner,requires gulp-nodemon
+
+gulp.task('default', function () {
+    nodemon({
+        script: 'app.js',
+        ext: 'js',
+        env: {
+            PORT: 8000
+        },
+        ignore: ['./node_module/**']
+    })
+        .on('restart', function () {
+            console.log('Restarting');
+        });
+});
 
 
 
